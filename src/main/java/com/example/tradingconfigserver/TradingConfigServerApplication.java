99c68954b6fba7a054886ded6ceb6d3eac768a84
JUKI-J <jju.ki@hotmail.com>
2022-02-15 14:50:05 +0900

    remove eureka

--- src/main/java/com/example/tradingconfigserver/TradingConfigServerApplication.java
+++ src/main/java/com/example/tradingconfigserver/TradingConfigServerApplication.java
@@ -3,10 +3,9 @@ package com.example.tradingconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class TradingConfigServerApplication {
 
